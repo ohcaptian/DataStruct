f2311bfa02f88d9bf9a4b62979e0ad9161f69df5
cz_captain <cz_captain@163.com>
2020-05-22 18:23:32 +0800

    使用C++实现双链表

--- Struct/DuLinkList.cpp
+++ Struct/DuLinkList.cpp
@@ -8,19 +8,12 @@ class Node{
 public:
     T data;
     Node *prev,*next;
-    Node(){
+    Node(T v, Node *n, Node *p){
+        this->data=v;
+        this->next=n;
+        this->prev=p;
+    }
 
-    }
-    Node(T val ,Node *pre = nullptr, Node *next = nullptr){
-        this->data=val;
-        this->prev=pre;
-        this->next= next;
-    }
-//    DuLinkListNode(T t, DuLinkListNode *prev, DuLinkListNode *next){
-//            this->data =t;
-//            this->next=prev;
-//            this->next=next;
-//    }
 };
 template<typename T>
 class MyDuLinkList{
@@ -29,70 +22,126 @@ public:
         Node<T> *head;
         Node<T> *tail;
 public:
-    MyDuLinkList();
+    MyDuLinkList(){
+        head = nullptr;
+        tail = nullptr;
+        size=0;
+    }
 
     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-    int get(int index);
+    int get(int index){
+        Node<T>* cur = head;
+            if(index>size||index<0){
+                cout<<"ERROR"<<endl;
+                return 1;
+            }
+            for(int i=0;i<index;i++){
+                cur = cur->next;
+            }
+            cout<<cur->data;
+            return 0;
+    }
 
 
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-    void addAtHead(T val) ;
+    void addAtHead(T val) {
+                if(!head){
+                    head = new Node<T>(val,nullptr, nullptr);
+                    tail  =head;
+                } else{
+                    Node<T>* temp  = new Node<T>(val, head, nullptr);
+                    head->prev = temp;
+                    head = temp;
+                }
+                    size++;
+
+    }
 
     /** Append a node of value val to the last element of the linked list. */
-    void addAtTail(T val) ;
+    void addAtTail(T val) {
+             if(!head){
+                 head = new Node<T>(val,nullptr, nullptr);
+                 tail  =head;
+             } else{
+                 Node<T>* temp = new Node<T>(val, nullptr,tail);
+                 tail->next = temp;
+                 tail = temp;
+             }
+            size++;
+    }
 
 
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-    void addAtIndex(int index, int val) ;
+    void addAtIndex(int index, int val) {
+                if(!head||index>size){
+                    cout<<"ERROR"<<endl;
+                    return;
+                }
+                Node<T> *cur = head;
+                Node<T>*pre = nullptr;
+                int temp =0;
+                while(cur){
+                    if(index==temp){
+                        Node<T>* newNode = new Node<T>(val,cur,pre);
+                        if (pre != nullptr) pre->next = newNode;
+                        cur->prev = newNode;
+                        size++;
+                        return;
+                    }
+                    pre = cur;
+                    cur = cur->next;
+                    temp++;
+                }
+    }
 
     /** Delete the index-th node in the linked list, if the index is valid. */
-    void deleteAtIndex(int index) ;
-
-    void display();
-};
-template<typename T>
-MyDuLinkList<T>:: MyDuLinkList(){
-    head = new Node<T>();//带头节点链表;
-    head->prev = head->next = nullptr;
-    size=0;
-}
-template<typename T>
-void MyDuLinkList<T>::addAtTail(T val) {
-    Node<T>* temp;
-    Node<T>* neNode;
-    head->next = temp;
-        neNode = new Node<T>(val);
-        neNode->prev=
-        head->prev = head;
-        size++;
-        return;
-
-}
-template<typename T>
-void MyDuLinkList<T>::addAtHead(T val) {
-    Node<T> *p = new Node<T>(val, head, head->next);
-    head->next->prev = p;
-    head->next = p;
-    size++;
-}
-template<typename T>
-void MyDuLinkList<T>::display()  {
-    Node<T>* temp;
-    temp = head->next;
-        if(!temp){
-            cout<<"List is empty";
+    void deleteAtIndex(int index) {
+        if(index>size||!head){
+            cout<<"ERROR"<<endl;
             return;
         }
-        while(temp!=head){
-            cout<<temp->data<<" ";
-           temp = temp->next;
+        int temp=0;
+        Node<T>* cur  = head;
+        Node<T>* pre;
+        while(cur){
+            if(temp==index){
+                Node<T> *old = cur;
+                if (pre!= nullptr) pre->next = cur->next;
+                if (cur->next != nullptr) cur->next->prev = pre;
+                delete old;
+                size--;
+                return;
+            }
+            temp++;
+            pre = cur;
+            cur = cur->next;
+            temp++;
         }
-        cout<<endl;
-}
+
+
+
+    }
+
+    void display(){
+                Node<T>* cur ;
+                cur = this->head;
+                while(cur){
+                    cout<<cur->data<<" ";
+                    cur = cur->next;
+                }
+                cout<<endl;
+    }
+};
 int main(){
     MyDuLinkList<int> duList;
     duList.addAtHead(1);
     duList.addAtHead(3);
+    duList.addAtHead(88);
+    duList.addAtTail(199);
     duList.display();
-
+    duList.addAtIndex(1,7);
+    duList.display();
+    duList.deleteAtIndex(2);
+    duList.display();
+    duList.get(3);
 }
--- Struct/Dulist.cpp
+++ Struct/Dulist.cpp
@@ -69,6 +69,13 @@ void myLinkedListAddAtHead(MyLinkedList &obj, int val) {
 }
 /** Append a node of value val to the last element of the linked list. */
 void myLinkedListAddAtTail(MyLinkedList obj, int val) {
+            MyLinkedList  head = obj->next;
+            //position tail;
+            while(head){
+                head = head->next;
+            }
+            MyLinkedList temp = new Node();
+            temp->
 
 }
 /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
