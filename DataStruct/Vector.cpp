dd5f6a3c472ed5e894b4eaa1639948f5ca03c7ca
cz_captain <cz_captain@163.com>
2020-05-25 20:08:29 +0800

    二叉树的层序遍历

---
+++ DataStruct/Vector.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 17694 on 2020/5/24.
+//
+template<typename T>
+class Vector{
+    private:
+        T* data;
+        int size;
+        int capcity;
+    public:
+    //初始化
+        Vector(){
+            data = nullptr;
+            size =0;
+            capcity = 0;
+        }
+
+
+};
