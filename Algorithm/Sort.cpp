76979ffa890a7e4c90a293bed122e310a8a630ba
cz_captain <cz_captain@163.com>
2020-05-30 16:09:52 +0800

    两种排序和完善了HuffmanTree

--- Algorithm/Sort.cpp
+++ Algorithm/Sort.cpp
@@ -2,3 +2,52 @@
 // Created by 17694 on 2020/5/25.
 //
 
+/*
+ * des  排序算法
+ */
+#include <iostream>
+using namespace std;
+#define  SWAP(a,b)     {int temp = a;a=b;b=temp;}
+void Bubble_Sort(int elem[],int length);
+void insert_sort(int elem[],int length);
+int main(){
+    int length=10;//排序数组大小
+    int elem[10]={2,5,675,8,21,66,22,12,54,87};
+    //Bubble_Sort(elem,length);
+    insert_sort(elem,length);
+}
+
+//冒泡拍序
+void Bubble_Sort(int elem[],int length){
+
+        for(int i=0;i<length-1;i++){
+            for(int j=0;i<length-1-j;j++){
+                if(elem[j]>elem[j+1]){
+                    SWAP(elem[j],elem[j+1]);
+                }
+            }
+        }
+        for(int i=0;i<length;i++){
+            cout<<elem[i]<<" ";
+        }
+        cout<<endl;
+}
+//插入排序
+void insert_sort(int elem[],int length){
+    int j;
+    for(int i=1;i<length;i++){
+        int temp = elem[i];
+        // 插入 每次从数组中取出一个数
+        /*
+         * 从当前有的元素的最后一个开始比较
+         */
+       for(j=i;j>0&&elem[j-1]>temp;j--){
+                    elem[j]=elem[j-1];//数组向后移;
+                 }
+             elem[j] = temp;
+    }
+    for(int i=0;i<length;i++){
+        cout<<elem[i]<<" ";
+    }
+    cout<<endl;
+}
--- DataStruct/HuffMan.cpp
+++ DataStruct/HuffMan.cpp
@@ -13,7 +13,9 @@ typedef struct Node{
     int weight;
     int parent ,lchild,rchild;
 }HFNode,*HuffMan;
+void Select_three(HuffMan ht,int n,int &s1,int &s2);
 void Select(HuffMan &HT, int n, int &s1, int &s2);
+void Select_two(HuffMan &HT, int n, int &s1, int &s2);//另一种选择方式
 void Swap(int &a,int &b);//交换两个值
 void CreateHuffMan(HuffMan &ht,int n);//生成HuffMan tree
 void CreateHuffManCode(HuffMan ht,Huffmancode &hc,int n);//哈夫曼编码
@@ -25,13 +27,12 @@ int main(){
         for(int i=1;i<2*n;i++){
             cout<<ht[i].weight<<" ";
         }
+        cout<<endl;
     CreateHuffManCode(ht,hc,n);
     for(int i=0;i<n;i++){
         cout<<hc[i]<<endl;
     }
 }
-
-
 void CreateHuffMan(HuffMan &ht,int n){
     int s1,s2;
     int m = 2*n;
@@ -46,7 +47,9 @@ void CreateHuffMan(HuffMan &ht,int n){
     }
     //将剩余的n-1个结点用来构建二叉树;
     for(int i=n+1;i<m;i++){
-        Select(ht,i-1,s1,s2);
+        //Select(ht,i-1,s1,s2);
+        //Select_another(ht,i-1,s1,s2);
+        Select_three(ht,i-1,s1,s2);
         ht[i].weight=ht[s1].weight+ht[s2].weight;
         ht[s1].parent=ht[s2].parent=i;
         ht[i].lchild=s1;
@@ -58,13 +61,13 @@ void CreateHuffMan(HuffMan &ht,int n){
  */
 //s1 ,s2 作为返回结点的下标;
 
-/*void Select(HuffMan ht,int n,int &s1,int &s2){
+void Select_three(HuffMan ht,int n,int &s1,int &s2){
         int m1,m2;//作为保存最小值的变量
-        m1 = ht[1].weight;//假设第一个值为最小值;
+        m1 = 999;//假设第一个值为最小值;
         m2 = m1;
         s1 = 1;
         s2 =s1;
-    for(int i=1;i<n;i++) {
+    for(int i=1;i<=n;i++) {
         if (ht[i].parent == 0) {
             if (ht[i].weight < m2) {
                 m2 = ht[i].weight;
@@ -81,7 +84,8 @@ void Swap(int &a,int &b){
     int temp = a;
     a = b;
     b  = temp;
-}*/
+}
+
 void Select(HuffMan &HT, int n, int &s1, int &s2)
 {
     // 7 5 3 2 ; n  =4;
@@ -111,6 +115,25 @@ void Select(HuffMan &HT, int n, int &s1, int &s2)
                 minum = i;
     }
     s2 = minum;
+}
+//7 5 3 2 5 10 17
+void Select_two(HuffMan &HT, int n, int &s1, int &s2){
+                int ml,mr;
+                ml = mr =999;
+                for(int i=1;i<=n;i++){
+                    if(HT[i].parent==0){
+                        if(HT[i].weight<ml){
+                            mr =ml;
+                            ml = HT[i].weight;
+                            s2 =s1;
+                            s1 = i;
+                        } else if(HT[i].weight<mr)
+                            {
+                            mr = HT[i].weight;
+                            s2  =i;
+                        }
+                    }
+                }
 }
 void CreateHuffManCode(HuffMan ht,Huffmancode &hc,int n){
             char *cd;
